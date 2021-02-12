a319ee7bb7a4f3b0c740e629eb9f311630092c32
Andres Roldan <dresroldan@gmail.com>
2021-02-12 10:01:55 -0600

    added styling to project cards and layout with flexbox

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "../styles/projectcard.scss";
+
 
 class Card extends React.Component {
 
@@ -14,32 +16,17 @@ class Card extends React.Component {
             <div className="card-body">
               <h2>{this.props.title}</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-              <h5>{this.props.author}</h5>
+              
+              <button>{this.props.link}</button>
+              <button>{this.props.linktwo}</button>
             </div>
           </div>
       )
     }
   }
   
-  class App extends React.Component {
-    render() {
-      return (
-        <>
-         <div class="header">
-           <h1>React Card Component</h1>
-           <h3>A simple and responsive card component in React with clean hover effects. Copy and customise.</h3>
-         </div>
-        
-         <div className='cards'>
-           <Card
-            img='https://picsum.photos/id/54/400/300'
-            title='What I learned from my visit to The Upside Down'
-            author='Nancy Wheeler' />
-        </div>  
-        </>
-      )
-    }
-  }
+  
+  
   
 
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,27 +1,52 @@
 import React from "react";
-import "../styles/projectcard.scss";
-import Card from "./ProjectCard"
+// import "../styles/projectcard.scss";
+import Card from "./ProjectCard";
+import "../styles/projects.scss";
+
+
 
 const Projects = () => {
   return (
     <section id="projects">
       <div className="container">
-      
-
         <div className="projects-section">
-        <div className="projects-title">
-          <h2>_projects</h2>
-        </div>
+          <div className="projects-title">
+            <h2>_projects</h2>
+</div>
+
+
+
           <div className="project-content">
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+            <Card
+              img="https://picsum.photos/id/54/400/300"
+              title="What I learned from my visit to The Upside Down"
+              link="demo"
+              linktwo="code"
+            />
+
+            <Card
+              img="https://picsum.photos/id/54/400/300"
+              title="What I learned from my visit to The Upside Down"
+              link="demo"
+              linktwo="code"
+            />
+            <Card
+              img="https://picsum.photos/id/54/400/300"
+              title="What I learned from my visit to The Upside Down"
+              link="demo"
+              linktwo="code"
+            />
+            <Card
+              img="https://picsum.photos/id/54/400/300"
+              title="What I learned from my visit to The Upside Down"
+              link="demo"
+              linktwo="code"
+            />
           </div>
+        
+        
+        
         </div>
-      
-      
-      
       </div>
     </section>
   );
