035c95801ef6480f537ba1fa025da35165c5d217
Andres Roldan <dresroldan@gmail.com>
2021-03-09 22:15:15 -0600

    updated about me section with new photo and integrated css grid for layout

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "../styles/about.scss";
-import img from "../assets/images/portfoliophoto2.jpg";
+import img from "../assets/images/andresfullimage.jpg";
 
 const About = () => {
   return (
     <section id="about">
       <div className="container">
         <div className="about-wrapper">
-          <h2>_about</h2>
+          <div className="image-content">
+            {/* <h2>_about</h2> */}
+            <img src={img}></img>
+          </div>
 
-          <div className="about-heading">
+          <div className="info-text">
+            <h2>Andres Roldan</h2>
             <p>
               Front end developer with a background in music production and an
               eye for design. Recent certificate in full stack development from
@@ -19,60 +23,42 @@ const About = () => {
               applications that improve the lives of others through user
               experience.
             </p>
-          </div>
 
-          <div className="contact-wrapper">
-            <div className="image-content">
-              <img src={img}></img>
+            <div className="linkss">
+              <ul className="fa-ul">
+                <li>
+                  <span className="fa-li">
+                    <i className="fas fa-envelope"></i>
+                  </span>
+                  dresroldan@gmail.com
+                </li>
+
+                <li>
+                  <span className="fa-li">
+                    <i className="fas fa-phone"></i>
+                  </span>
+                  779.207.8144
+                </li>
+              </ul>
             </div>
 
-            <div className="contact-info">
-              <div className="links">
-                <ul className="fa-ul">
-                  <li>
-                    <span className="fa-li">
-                      <i className="fas fa-user"></i>
-                    </span>
-                    Andrés Roldán
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <i className="fas fa-envelope"></i>
-                    </span>
-                    dresroldan@gmail.com
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <i className="fas fa-globe"></i>
-                    </span>
-                    Chicago, IL
-                  </li>
-                  <li>
-                    <span className="fa-li">
-                      <i className="fas fa-phone"></i>
-                    </span>
-                    779.207.8144
-                  </li>
-                </ul>
+            <div className="buttons">
+              <a
+                className="button"
+                href="https://drive.google.com/file/d/1Rbd3QmBiwz_b83c-k0Lr7RLEsWS2-D6q/view?usp=sharing"
+              >
+                _resume
+              </a>
+              <a className="button" href="https://github.com/dresroldan">
+                _github
+              </a>
 
-                <a
-                  className="button"
-                  href="https://drive.google.com/file/d/1Rbd3QmBiwz_b83c-k0Lr7RLEsWS2-D6q/view?usp=sharing"
-                >
-                  _resume
-                </a>
-
-                <a className="button" href="https://github.com/dresroldan">
-                  _github
-                </a>
-
-                <a
-                  className="button"
-                  href="https://www.linkedin.com/in/andresroldan23/"
-                >
-                  _linkedin
-                </a>
-              </div>
+              <a
+                className="button"
+                href="https://www.linkedin.com/in/andresroldan23/"
+              >
+                _linkedin
+              </a>
             </div>
           </div>
         </div>
