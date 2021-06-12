2774efb94286d375a5601e8b8af4989a90dee0b5
Andres Roldan <dresroldan@gmail.com>
2021-06-12 16:23:18 -0500

    Refactor header and header.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import '../styles/header.scss';
 import ArrowButton from './ArrowButton';
+import '../styles/header.css';
 
 const Header = () => {
   return (
-    <header id="home">
-      <div className="container">
-        <div className="header-wrapper">
+    <header>
+      <div className="header__container">
+        <div className="header__wrapper">
           <h1>
             Hello,
             <br />
             I'm Andrés
           </h1>
-          <p>
+          <p className="header__subheading">
             Chicago-based developer focused on building clean, and user-friendly
             web experiences using modern web technologies such as React.
           </p>
-
           <ArrowButton />
         </div>
       </div>
