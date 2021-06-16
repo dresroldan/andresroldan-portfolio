b49c11da81ed763dff0c40be2483d001ab0733f9
Andres Roldan <dresroldan@gmail.com>
2021-06-16 08:19:58 -0500

    Refactor skills.js and skills.css

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import '../styles/skills.scss';
+import '../styles/skills.css';
 
 const Skills = () => {
   return (
     <section id="skills">
       <div className="container">
-        <div className="skills-wrapper">
-          <h2>skill-set</h2>
+        <div className="skills__wrapper">
+          <h4>skill-set</h4>
 
-          <div className="skills-list">
+          <div className="skills__list">
             <p>
               HTML5, JavaScript, ES6, jQuery, CSS, SCSS, Bootstrap, Material UI,
-              React JS, JSX, Mobile Responsive Design, Node.js, MySQL,
+              React JS, Redux, JSX, Mobile Responsive Design, Node.js, MySQL,
               ExpressJS, MongoDB, JSON, REST APIs, AJAX, NPM, Testing,
               Handlebars, GIT, GitHub, GitLab, Heroku, Postman, Figma, Trello,
               Google Workspace
