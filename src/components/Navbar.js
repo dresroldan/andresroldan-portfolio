c15012f124a69f7488b4d343696d735e82684fc3
Andres Roldan <dresroldan@gmail.com>
2021-12-09 18:08:44 -0600

    Refactorr

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import '../styles/navbar.css';
 
 const Navbar = () => {
+  const [isActive, setActive] = useState('true');
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
+
   return (
-    <nav>
-      <div className="container">
-        <div className="navbar__wrapper">
-          <Link
-            activeClass="active"
-            to="about"
-            spy={true}
-            smooth={true}
-            duration={1000}
-            className="navbar__title"
-          >
-            Andrés Roldán
-          </Link>
-
-          <div className="navbar__links">
-            <Link
-              activeClass="active"
-              to="about"
-              spy={true}
-              smooth={true}
-              duration={1000}
-              className="navbar__link"
-            >
-              about
-            </Link>
-
-            <Link
-              activeClass="active"
-              to="projects"
-              spy={true}
-              smooth={true}
-              duration={1000}
-              className="navbar__link"
-            >
-              projects
-            </Link>
-          </div>
-        </div>
+    <header>
+      <div className="header__group left">
+        <Link
+          activeClass="active"
+          to="about"
+          spy={true}
+          smooth={true}
+          duration={1000}
+          className="header__logo"
+        >
+          Andrés Roldán
+        </Link>
       </div>
-    </nav>
+
+      <div className="header__group right">
+        <div className="hamburger" onClick={handleToggle}>
+          <div className="hamburger__line"></div>
+          <div className="hamburger__line"></div>
+        </div>
+
+        <nav role="navigation" className={isActive ? 'navbar--hidden' : null}>
+          <div className="navbar__menu">
+            <div className="hamburger__menu" onClick={handleToggle}>
+              <div className="hamburger__menu-line"></div>
+              <div className="hamburger__menu-line"></div>
+            </div>
+
+            <div className="navbar__menu-icon"></div>
+            <ul className="navbar__list">
+              <li className="navbar__list-item">
+                <Link
+                  activeClass="active"
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  duration={1000}
+                  className={isActive ? 'navbar__link' : 'navbar__link--menu'}
+                >
+                  projects
+                </Link>
+              </li>
+
+              <li className="navbar-list-item">
+                <Link
+                  activeClass="active"
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  duration={1000}
+                  className={isActive ? 'navbar__link' : 'navbar__link--menu'}
+                >
+                  projects
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
   );
 };
 
